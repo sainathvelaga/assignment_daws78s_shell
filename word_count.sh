ab1bb91eb1c1b7aec6bb067ef8d175db789f77ea
sainath <sainath.velaga@gmail.com>
2024-08-31 20:32:17 +0530

    Assignments

--- word_count.sh
+++ word_count.sh
@@ -1,6 +1,5 @@
-/* 
-write a shell script that reads a text file and count occurrences of each word and display 
-top most words along with their counts
-*/
+
+#write a shell script that reads a text file and count occurrences of each word and display 
+#top most words along with their counts
 
 grep -o '\w*' /home/ec2-user/assignment_daws78s_shell/sample.txt | sort | uniq -c | sort -nr
